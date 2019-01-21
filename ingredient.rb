0d94ce61f6207248ebbc31e7f5749f4e4e7c12f9
brandong1 <brandon@brandonleeproductions.com>
2019-01-21 01:48:32 -0700

    created first two methods

--- ingredient.rb
+++ ingredient.rb
@@ -1,4 +1,29 @@
 class Ingredient
     attr_accessor :type, :stock, :daily
 
+    def amount_needed
+        @daily * 7 - @stock
+    end
+
+    inventory = []
+
+    while user_input = gets.strip
+
+    end
+
+    def info
+        puts "'1' to add an ingredient.
+        '2' to print out shopping list. 
+        '3' to exit."
+    end
+
+    info
+
+    while user_input = gets.strip
+
+        case user_input
+            when "1"
+                add_new_ingredient inventory
+
+
 end
