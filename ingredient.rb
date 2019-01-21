30e758150614b5132353a060340494aabf32de31
brandong1 <brandon@brandonleeproductions.com>
2019-01-21 02:11:53 -0700

    added additional methods.

--- ingredient.rb
+++ ingredient.rb
@@ -1,29 +1,35 @@
 class Ingredient
+    #Define the attr_accessor attributes for type of ingredient, amount in stock, and daily consumption.
 	attr_accessor :type, :stock, :daily
-	def amount_needed 
+    
+    #Define an instance method in the class for calculating how much is needed for the next seven days.
+    def amount_needed 
 		@daily * 7 - @stock
 	end
 
 end
 
+#Define an array that will hold an ingredient in each slot.
 inventory = []
+    
+    #Define a method for adding an ingredient to the inventory. Be sure to prompt the user and record their input for type, stock, and daily use.
+    def add_new_ingredient inventory
+        new_ingredient = Ingredient.new
+        puts "Type of ingredient"
+        new_ingredient.type = gets.strip
+        puts "Current Stock"
+        new_ingredient.stock = gets.to_i
+        puts "Daily Amount Required"
+        new_ingredient.daily = gets.to_i
+        inventory[inventory.size] = new_ingredient
+    end
 
-def add_new_ingredient inventory
-	new_ingredient = Ingredient.new
-	puts "Type of ingredient"
-	new_ingredient.type = gets.strip
-	puts "Current Stock"
-	new_ingredient.stock = gets.to_i
-	puts "Daily Amount Required"
-	new_ingredient.daily = gets.to_i
-	inventory[inventory.size] = new_ingredient
-end
-
-def shopping_list inventory
-	inventory.each do |ingredient|
-		puts "#{ingredient.type} \t\t\t #{ingredient.amount_needed}"
-	end
-end
+    #Define a method for printing out the shopping needs for the next week.
+    def shopping_list inventory
+        inventory.each do |ingredient|
+            puts "#{ingredient.type} \t\t\t #{ingredient.amount_needed}"
+        end
+    end
 
 def info
 	puts "'1' to add an ingredient. '2' to print out shopping list '3' to exit"
